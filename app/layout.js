7f7d4ade43706ded5c34f16487798a3c29157b51
Kongkon Jowarder <kongkon4545@gmail.com>
2024-04-21 20:36:48 +0600

    json-server create

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={inter.className}>
         {/* <nav>
           <ul className="flex gap-10 justify-center items-center h-12 bg-blue-500">
